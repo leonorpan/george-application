cc6c9ef1cf057e0cfe71698b6b52750b5ad9e36d
Terje Dahl <terje@andante.no>
2016-01-06 20:38:51 +0100

    highlighting that works.  Hovering over tokens or parens glows them.

--- src/main/clojure/dev/highlight.clj
+++ src/main/clojure/dev/highlight.clj
@@ -16,33 +16,21 @@
         )
     (:import
         [java.util Collections]
-        ;[dev.reader SyntaxError]
-        [org.fxmisc.richtext CodeArea LineNumberFactory StyleClassedTextArea StyleSpansBuilder]
-        )
+        [org.fxmisc.richtext  LineNumberFactory StyledTextArea  MouseOverTextEvent]
 
-    )
+        ))
+
 
 
 (fxc/import!)
 
-
-
-(defn- make-code-area []
-    (let [
-            area
-                (doto
-                    (StyleClassedTextArea. false)
-                    (.setStyle "
-                            -fx-font: 14 'Source Code Pro Regular';
-                            -fx-padding: 10 5;
-                            "))
-        ]
-        (doto area
-            (.setUseInitialStyleForInsertion true)
-            (-> .getUndoManager .forgetHistory)
-            (-> .getUndoManager .mark)
-            (.selectRange 0 0))))
-
+(defn- clamp
+    "ensures val does not exceed min or max"
+    [min val max]
+    (cond
+        (< val min) min
+        (> val max) max
+        :else val))
 
 
 (defn- read-sample-code[]
@@ -50,13 +38,66 @@
 
 
 
+(defrecord MyStyle [color weight underline hover])
 
-(defn- style-class [area  length css-class ranges]
+
+(defn- style->text [^Text text ^MyStyle style]
+    (let [{c :color w :weight u :underline h :hover b :background} style]
+        (doto text
+            (.setStyle
+                (str
+                    "-fx-fill: " (if h " gold" (if c c "black")) ";"
+                    "-fx-font-weight: " (if w w "normal") ";"
+                    "-fx-underline: " (if u u "false") ";"
+                    "-fx-effect: " (if h "dropshadow(one-pass-box, slategray, 5, 1, 0, 0)" "null") ";"
+                    ))
+            (.setCursor (if h Cursor/DEFAULT nil))
+        )))
+
+
+(defn- style-text-function []
+    (reify
+        java.util.function.BiConsumer
+        (accept [this text style] ;;  ^Text ^MyStyle
+            (style->text text style))))
+
+
+(defn- my-style-text-area []
+    (doto (StyledTextArea.
+              (->MyStyle nil nil nil nil)
+              (style-text-function))
+        (.setStyle "
+            -fx-font: 16 'Source Code Pro Medium';
+            -fx-padding: 10 5;")
+
+        (.setUseInitialStyleForInsertion true)
+        (-> .getUndoManager .forgetHistory)
+        (-> .getUndoManager .mark)
+        (.selectRange 0 0)
+
+        ))
+
+
+(defn- set-style [text-area text-length ranges style is-hover]
+    (doseq [[from to] ranges]
+        (let [
+                 ;; ensure bounds
+                 new-style  (assoc style :hover is-hover)
+                 from (max 0 from)
+                 to (min to text-length)
+                 ]
+            (println "new-style:" from to new-style)
+            (.setStyle text-area from to new-style)
+
+            )))
+
+
+(comment defn- style-class [area  length css-class ranges]
     (doseq [[from to] ranges]
         (let [
                 ;; ensure bounds
-                 from (if (< from 0) 0 from)
-                 to (if (> to length) length to)
+                 from (max 0 from)
+                 to (min to length)
              ]
             (println "style-class:" css-class from to)
             (.setStyleClass area from to css-class))))
@@ -69,22 +110,36 @@
         :else [[from to]]
     ))
 
-(defn- type->css-class [typ]
-    (println "type->css-class")
+
+(defn- type->style [typ]
+    (println "type->style")
     ;(println "  ## typ:" typ)
-    (cond
-        (isa? typ java.lang.Number)  "teal"
-        (#{String Character} typ) "green"
-        (isa? typ java.lang.Iterable)   "blue"
-        (= typ clojure.lang.Keyword)  "pink"
-        (#{clojure.lang.Symbol}  typ) "black"
-        (= typ :syntax-error) "syntax-error"
-        (= typ :reader-exception) "error"
-        :else "orange"
-    ))
+    (assoc
+        (->MyStyle
+            (cond
+                (isa? typ java.lang.Number)  "#005d69" ;;  teal
+                (#{String Character} typ) "#005500"  ;; green
+                (isa? typ java.lang.Iterable)   "blue"
+                (= typ clojure.lang.Keyword)   "#9918ff"  ;; dark pink
+                (#{clojure.lang.Symbol}  typ) "black"
+                (= typ :syntax-error) "#ff5500"  ;; orange
+                (= typ :reader-exception) "red"
+                :else "orange" )
+        nil
+        nil
+        false)
+        :underline (#{:syntax-error :read-exception} typ)))
 
 
-(defn- highlight-it [token code-area length]
+(defn- assoc-token
+    "assocs a given token to every index it covers"
+    [rngs token token-index]
+    (doseq [rng rngs]
+        (doseq [i (apply range rng)]
+            (swap! token-index assoc i token))))
+
+
+(defn- highlight-it [token code-area length token-index]
     (println "highlight-it")
     ;(println "  ## token:" token)
 
@@ -92,11 +147,13 @@
         (let [
                  d (.getData token)
                  {from :starting-index to :ending-index typ :type error :error} d
-                 c (type->css-class typ)
-             ]
+                style (type->style typ)
+                 ]
             (println  "  #### ExceptionInfo  type:" type " message:" (.getMessage token) " error:" error " data:" d)
             (when (not= :EOF error)
-                (style-class code-area length c (ranges typ from to )))
+                (set-style code-area length  (ranges typ from to) style false)
+
+                )
         ))
 
     (when-let [m (meta token)]
@@ -104,23 +161,24 @@
         (let [
                  {from :starting-index to :ending-index typ :type} m
                  typ (if typ typ (type token))
-                 c (type->css-class typ)
+                 style (type->style typ)
+                 rngs (ranges typ from to)
              ]
-            (style-class code-area length c (ranges typ from to ) ))))
+            (assoc-token rngs token token-index)
+            (set-style code-area length rngs style  false)
+            )))
 
 
-
-(defn- highlight-them [tokens code-area length]
+(defn- highlight-them [tokens code-area length token-index]
     (println "highlight-them")
     ;(println "  ## tokens:" tokens)
-    (highlight-it tokens code-area length)
+    (highlight-it tokens code-area length token-index)
     (when (coll? tokens)
         (doseq [token tokens]
-            (highlight-them token code-area length))))
+            (highlight-them token code-area length token-index))))
 
 
-
-(defn- highlight-code-2 [code-area old-code code]
+(defn- highlight-code [code-area old-code code token-index]
     "a second attempt at a reader/parser/higlighter"
     (if (not= old-code code)
         (let [
@@ -128,9 +186,11 @@
                  rdr (my/indexing-pushback-stringreader code)
              ]
             (. code-area clearStyle 0 length)
+            (reset! token-index (mapv (constantly nil) (range length)))
+
             (loop [res nil]
                 (when (not= res :eof)
-                    (if res (highlight-them res code-area length))
+                    (if res (highlight-them res code-area length token-index))
                     (Thread/sleep 10)
                     (let [
                              res (try
@@ -160,28 +220,74 @@
                         (recur res)))))))
 
 
-(defn- code-area-change-listener [code-area]
+
+(defn- code-area-change-listener [code-area token-index]
     (reify ChangeListener
         (changed [_ obs old-txt new-txt]
-            (highlight-code-2 code-area old-txt new-txt))))
+            (highlight-code code-area old-txt new-txt token-index)
+            (doseq [i (range (count @token-index))]
+                (let [t (get @token-index i) m (meta t)]
+                    (println i ": " (type t) "" m )))
+        )))
+
+
+(defn- token->ranges [token]
+    (let [
+             m (meta token)
+             {from :starting-index to :ending-index typ :type} m
+             typ (if typ typ (type token))
+         ]
+         (ranges typ from to)))
+
+
+(defn- mouse-over-handler [text-area token-index ]
+    (let [last-hovered-index (atom nil)]
+
+        (fx/event-handler-2 [_ event]
+            (when-let [i @last-hovered-index]
+                (let [
+                        old-style (. text-area getStyleOfChar i)
+                        token (get @token-index i)
+                    ]
+                    (reset! last-hovered-index nil)
+                    (set-style text-area (. text-area getLength) (token->ranges token) old-style false)
+
+                    ))
+
+            (let [
+                    i (. event getCharacterIndex)
+                    old-style (. text-area getStyleOfChar i)
+                    token (get @token-index i)
+                ]
+                (reset! last-hovered-index i)
+                (set-style text-area (. text-area getLength) (token->ranges token) old-style true)
+
+            ))))
+
 
 
 (defn- build-scene []
     (let [
-            area (make-code-area) ;(CodeArea.)
-            linenumber-factory (LineNumberFactory/get area)
-            _ (.setParagraphGraphicFactory area linenumber-factory)
+            area (my-style-text-area)
+            _ (def area area)
+             linenumber-factory (LineNumberFactory/get area)
+             _ (.setParagraphGraphicFactory area linenumber-factory)
 
-            sample-code (read-sample-code)
-            _ (.replaceText area 0 0 sample-code)
+             sample-code (read-sample-code)
 
-            scene (Scene. (StackPane. (j/vargs area)) 600 400)
+             scene (Scene. (StackPane. (j/vargs area)) 600 400)
+             token-index (atom []) ;; this will contain references to parse-results by index - for lookups
+            hover-handler (mouse-over-handler area token-index)
+]
+        ; (fx/add-stylesheets scene "styles/basic.css" "dev/highlight/code.css" )
+        (fx/add-stylesheets scene "styles/basic.css" )
 
-            ]
-        (fx/add-stylesheets scene "styles/basic.css" "dev/highlight/code.css" )
-
-        (-> area .textProperty (.addListener (code-area-change-listener area)))
-        (highlight-code-2 area nil (. area getText))
+        (doto area
+            (.setMouseOverTextDelay (java.time.Duration/ofMillis 100))
+            (.addEventHandler MouseOverTextEvent/MOUSE_OVER_TEXT_BEGIN hover-handler)
+            (-> area .textProperty (.addListener (code-area-change-listener area token-index)))
+            (.replaceText area 0 0 sample-code)
+        )
 
         scene ))
 
