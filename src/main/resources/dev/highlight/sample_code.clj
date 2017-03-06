0de77f0268961f24a2c143f9372736a730f1f68e
Terje Dahl <terje@andante.no>
2017-03-06 15:23:25 +0100

    Added copyright notice to all code files.

--- src/main/resources/dev/highlight/sample_code.clj
+++ src/main/resources/dev/highlight/sample_code.clj
@@ -1,5 +1,10 @@
 
- (+ 1.1 2 (+ 3 4) 5)
+ ;  Copyright (c) 2017 Terje Dahl. All rights reserved.
+; The use and distribution terms for this software are covered by the Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php) which can be found in the file epl-v10.html at the root of this distribution.
+;  By using this software in any fashion, you are agreeing to be bound by the terms of this license.
+;  You must not remove this notice, or any other, from this software.
+
+(+ 1.1 2 (+ 3 4) 5)
   ( - 8 7) {:aaa "AAA" ::bb "BB"}
 [\a \b \c \newline]
 #{ "T" "D"}
