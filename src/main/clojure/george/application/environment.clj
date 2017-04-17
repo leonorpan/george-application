00db088ffb457f627bfc4c053253e9a343bc3f67
Terje Dahl <terje@andante.no>
2017-04-18 01:18:15 +0200

    Re-introduces pieces that were lost in previous merge (due to filename changes).

--- src/main/clojure/george/application/environment.clj
+++ src/main/clojure/george/application/environment.clj
@@ -4,15 +4,18 @@
 ;  You must not remove this notice, or any other, from this software.
 
 (ns
-    ^{:author "Terje Dahl"}
-    george.application.environment
-    (:require
-        [clojure.java.io :refer [file] :as cio]
-        [george.javafx :as fx]
-        [george.application.turtle.turtle :as tr]
-        [george.core.core :as gcc]
-        [george.util.singleton :as singleton]
-        [george.application.code :as code]))
+  ^{:author "Terje Dahl"}
+  george.application.environment
+  (:require
+    [clojure.string :as cs]
+    [clojure.java.io :refer [file] :as cio]
+    [george.javafx :as fx]
+    [george.application.turtle.turtle :as tr]
+    [george.core.core :as gcc]
+    [george.util.singleton :as singleton]
+    [george.application.code :as code]
+    [george.javafx.java :as fxj])
+  (:import (javafx.scene Node)))
 
 
 
@@ -20,11 +23,60 @@
   (let [current-ns (:ns (meta #'prep-user-turtle-ns))]
     (binding [*ns* nil]
       ;; prep a user namespace
-      (ns user.turtle (:require [george.application.turtle.turtle :refer :all]))
+      (ns user.turtle
+        (:require [clojure.repl :refer :all])
+        (:require [clojure.pprint :refer [pprint]])
+        (:require [george.application.turtle.turtle :refer :all])
+        (:import [javafx.scene.paint Color]))
       ;; switch back to this namespace
       (ns current-ns))))
 
 
+
+(defn- doc-str [var]
+  (let [m (meta var)
+        n (str (:name m))
+        dc (:doc m)
+        argls (:arglists m)
+        combos (cs/join "  "
+                        (map #(str "("
+                                   n
+                                   (if (empty? %) ""  (str " " (cs/join " " %)))
+                                   ")")
+                             argls))]
+    (str combos \newline \newline dc)))
+
+
+(defn- turtle-commands-stage-create []
+  (let [commands tr/ordered-command-list
+        name-fn #(-> % meta :name str)
+        doc-fn doc-str
+        labels (map #(doto (fx/label (name-fn %)) (fx/set-tooltip (doc-fn %)))
+                    commands)
+        stage-WH [200 200]
+        screen-WH (-> (fx/primary-screen) .getVisualBounds fx/WH)]
+
+       (fx/now
+
+         (fx/stage
+           :title "Turtle commands"
+           :location [(- (first screen-WH) (first stage-WH) 10)
+                      (- (second screen-WH) (second stage-WH) 10)]
+           :sizetoscene false
+           :tofront true
+           :onhidden #(singleton/remove ::commands-stage)
+           :size stage-WH
+           :resizable false
+           :scene (fx/scene
+                    (fx/scrollpane
+                      (apply fx/vbox
+                             (concat (fxj/vargs-t* Node labels)
+                                     [:padding 10 :spacing 5]))))))))
+
+
+(defn- turtle-commands-stage []
+  (singleton/put-or-create
+    ::commands-stage turtle-commands-stage-create))
 
 
 (defn- toolbar-pane [is-turtle]
@@ -66,6 +118,11 @@
                             :onaction #(code/new-code-stage :namespace user-ns-str)
                             :tooltip "Open a new code editor")
 
+                 (fx/button "Commands"
+                            :width button-width
+                            :onaction  #(turtle-commands-stage)
+                            :tooltip "View list of available turtle commands")
+
                  :spacing 10
                  :padding 10)))]
      pane))
@@ -76,7 +133,7 @@
   (let [is-turtle (= ide-type :turtle)]
     (fx/now
       (fx/stage
-        :location [520 17]
+        :location [390 0]
         :title (if is-turtle "Turtle Geometry" "IDE")
         :scene (fx/scene (toolbar-pane is-turtle))
         :sizetoscene true
@@ -101,4 +158,4 @@
 
 ;;; DEV ;;;
 
-;(println "WARNING: Running george.application.turtle.environment/-main" (-main))
+;(println "WARNING: Running george.application.turtle.environment/-main" (-main :turtle))
