5faf74f41914844104d717699cd5c4e8676180cc
Terje Dahl <terje@andante.no>
2017-05-03 00:20:45 +0200

    Cleanup and adjustments to preloader.

--- src/main/java/no/andante/george/IStageSharing.java
+++ src/main/java/no/andante/george/IStageSharing.java
@@ -1,11 +1,7 @@
 package no.andante.george;
 
-import javafx.scene.Parent;
 
-/* Contact interface between application and preloader */
 public interface IStageSharing {
-    /* Parent node of the application */
-//    Parent getRootNode();
     void handover(javafx.stage.Stage stage);
 }
 
