508b4cde9bdcc117f4eda4e1f6da5c8c9b425ec7
Terje Dahl <terje@andante.no>
2017-12-20 07:19:21 +0100

    Reverting to version-file solution.

--- bin/build-jar.sh
+++ bin/build-jar.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
-# Ensure any old compiled classed are removed first.
+echo "!! Updating version string in src/main/resources ..."
+source bin/_utils.sh
+V=`get_version`
+VF=src/main/resources/george-version.txt
+echo $V > $VF
+cat $VF
 
+echo "!! Building ..."
+# Ensure any old compiled classed are removed first.
 lein clean
 lein uberjar
