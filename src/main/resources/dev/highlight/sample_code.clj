d2da81b00ad9af7e06a421892da779bf9f355af4
Terje Dahl <terje@andante.no>
2018-01-03 00:12:38 +0100

    Copyright dates. Image in README.

--- src/main/resources/dev/highlight/sample_code.clj
+++ src/main/resources/dev/highlight/sample_code.clj
@@ -1,11 +1,10 @@
+;; Copyright (c) 2016-2018 Terje Dahl. All rights reserved.
+;; The use and distribution terms for this software are covered by the Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php) which can be found in the file epl-v10.html at the root of this distribution.
+;; By using this software in any fashion, you are agreeing to be bound by the terms of this license.
+;; You must not remove this notice, or any other, from this software.
 
- ;  Copyright (c) 2017 Terje Dahl. All rights reserved.
-; The use and distribution terms for this software are covered by the Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php) which can be found in the file epl-v10.html at the root of this distribution.
-;  By using this software in any fashion, you are agreeing to be bound by the terms of this license.
-;  You must not remove this notice, or any other, from this software.
-
-(+ 1.1 2 (+ 3 4) 5)
-  ( - 8 7) {:aaa "AAA" ::bb "BB"}
+(+ 1.1 2 (+ 3 4) 5
+  ( - 8 7) {:aaa "AAA" ::bb "BB"})
 [\a \b \c \newline]
 #{ "T" "D"}
 1.1 1/2  45N
@@ -24,8 +23,8 @@ NaN  -Infinity Infinity +Infinity
 #(println "name:" % %2)
 \uE001
 \o17
-#"regex" {(}
-'a-symbol ([
+#"regex" {()}
+'a-symbol ([])
 ~arg1
 '~arg2
 `syn-qt
@@ -34,11 +33,11 @@ NaN  -Infinity Infinity +Infinity
 over lines!"
 
 genarated-sym#
- (
+ ()
  ( %)  ( %2) %13 %& %x
- )][
+ []
 (
-     ( () () )
+     ( () ())
      []
-     { () [] }
-)
+     { () []})
+
